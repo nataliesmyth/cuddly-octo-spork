172e5ee9a5e4911caedc307453ce87e7897e6ed6
Nataliesmyth <nataliesmyth11@gmail.com>
2021-09-20 07:11:44 -0400

    server

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const app = express;
 // if on a production server, use that port, if not use 4000
 const PORT = process.env.PORT || 4000;
 
-app.listen(3000, () => {
-    console.log('i am listening...')
-})
+
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
